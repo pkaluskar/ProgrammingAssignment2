8798e0999725006843e282e2a0145d2ffb844136
Pramod Kaluskar <pkaluskar@yahoo.com>
2015-05-24 12:38:04 -0400

    Fixed errors:   Variable names and usage.

--- cachematrix.R
+++ cachematrix.R
@@ -1,41 +1,52 @@
 ## makeCacheMatrix() and cacheSolve() are routines that optimize
 ## matrix operations by caching matrix-inversions.
 
-## makeCacheMatrix() creates an "object" that holds two variables
-## (the original "origMatrix" and its "inverse").
-## It also proivides 4 methods - set/getmatrix() and set/getcache()
-## to operate on these variables.
+## makeCacheMatrix() creates an "object" that holds the "inverse" of the matrix
+## It also proivides 3 methods - getmatrix() to extract the original
+## matrix; and set/getcache() to get and set the cached-inverse of the matrix.
 ##
-## cacheSolve() is called with the makeCacheMatrix-type matrix (mx);
-## and returns the inverse (either from the cache or computed)
-## 
 
 makeCacheMatrix <- function(x = matrix()) {
-  ## Data Objects
+  ## Data Objects:  Saved Inverse
   inverse <- NULL
   
-## Functions / Methods
-  setcache <- function(value) {
+  ## Functions / Methods
+  getmatrix <- function() {
+    x
+  }
+
+  setcache <- function( value ) {
     inverse <<- value
   }
+
   getcache <- function() {
     inverse
   }
-  list(setcache = setcache,   getcache = getcache)
+  
+  list(getmatrix = getmatrix,
+       setcache = setcache,
+       getcache = getcache)
 }
 
 
-## Write a short comment describing this function
+## cacheSolve() is called with the makeCacheMatrix-type matrix 
+## For eg:  x < matrix(1:9, 3,3)
+##  and mx <- makeCacheMatrix(x)]
+## It returns the inverse of the matrix (from the cache - if available, or from
+## the computation - if not)
+## 
 
 cacheSolve <- function(mx, ...) {
   ## Return a matrix that is the inverse of 'x'
   tmp <- mx$getcache()
-  # If value is found in the cache
-  if(!is.null(tmp)) {
-    return(inverse)
+  
+  # If value is found in the cache, return it
+  if( !is.null( tmp ) ) {
+    return( tmp )
   }
-  # Value not found in cache; compute
-  tmp <- solve(x)
-  mx$setcache(tmp)
+  # Value not found in cache; compute inverse, save it to cache, 
+  # and it return to caller
+  tmp <- solve( mx$getmatrix() )
+  mx$setcache( tmp )
   tmp
 }
